ae8976e2516992aece0768a419a578958fdefb92
Vitória Novaes <vitoria.m14@aluno.ifsc.edu.br>
2023-02-28 13:12:33 -0300

    Interface paciente

--- src/controller/InterfacePacienteDao.java
+++ src/controller/InterfacePacienteDao.java
@@ -7,5 +7,13 @@ import model.Paciente;
 public interface InterfacePacienteDao {
 	
 
+	public boolean cadastrarPaciente(Paciente paciente);
+	
+	public boolean excluirPaciente(Paciente paciente);
+	
+	public boolean alterarPaciente(Paciente paciente);
+	
+	public Paciente consultarPaciente(Paciente paciente);
+
 
 }
