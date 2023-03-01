02d8383fd34b688b4cbeb1728f9c37e7683a02e1
Eliezer <frete7132@gmail.com>
2023-02-28 23:40:36 -0300

    Interface login

--- src/view/TelaLogin.java
+++ src/view/TelaLogin.java
@@ -1,8 +1,12 @@
 package view;
 
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.RoundRectangle2D;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,10 +14,23 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 import controller.LoginDao;
 import model.Usuario;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.UIManager;
+import javax.swing.ImageIcon;
+import javax.swing.BorderFactory;
+import javax.swing.DropMode;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class TelaLogin extends JFrame {
 
@@ -46,32 +63,51 @@ public class TelaLogin extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaLogin() {
-		setExtendedState(MAXIMIZED_BOTH);
+
+		setTitle("Tela de Login");
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1103, 741);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(34, 139, 34));
+		contentPane.setForeground(new Color(0, 0, 102));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Login");
-		lblNewLabel.setBounds(239, 36, 46, 14);
-		contentPane.add(lblNewLabel);
+		setLocationRelativeTo(null);
+		setVisible(true);
 
-		txtUsuario = new JTextField();
+		txtUsuario = new RoundedJTextField(15);
+		txtUsuario.setForeground(new Color(0, 0, 51));
+		txtUsuario.setBackground(new Color(102, 204, 102));
 		txtUsuario.setText("Usuario\r\n");
-		txtUsuario.setBounds(163, 103, 249, 20);
+		txtUsuario.setBounds(162, 232, 249, 20);
 		contentPane.add(txtUsuario);
 		txtUsuario.setColumns(10);
 
-		txtSenha = new JTextField();
+		txtSenha = new RoundedJTextField(15);
+		txtSenha.setForeground(new Color(0, 0, 51));
+		txtSenha.setBackground(new Color(102, 204, 102));
 		txtSenha.setText("Senha");
-		txtSenha.setBounds(163, 147, 249, 20);
+		txtSenha.setBounds(162, 299, 249, 20);
 		contentPane.add(txtSenha);
 		txtSenha.setColumns(10);
 
+		// TODO alteracao no estilo do Botao
+
 		JButton btnNewButton = new JButton("Entrar");
+		btnNewButton.setVerticalAlignment(SwingConstants.TOP);
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		btnNewButton.setBackground(Color.LIGHT_GRAY);
+		btnNewButton.setBorder(new LineBorder(new Color(0, 128, 128), 10, true));
+		
+		btnNewButton.setBorder(null);
+		btnNewButton.setBorder(new BevelBorder(BevelBorder.LOWERED));
+		//btnNewButton.setBorder(BorderFactory.createBevelBorder(1, Color.black, Color.black));
+
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
@@ -79,44 +115,91 @@ public class TelaLogin extends JFrame {
 				String Senha = txtSenha.getText();
 				Usuario login = null;
 
-				if(!Usuario.isEmpty() && !Senha.isEmpty()) {
+				if (!Usuario.isEmpty() && !Senha.isEmpty()) {
 					LoginDao loginDao;
 					loginDao = new LoginDao();
-				
-				login = loginDao.consultarLogin(new Usuario(Usuario, Senha));
 
+					login = loginDao.consultarLogin(new Usuario(Usuario, Senha));
 
-				if (login != null) {
-					
-					MenuPrincipal telaFunc = new MenuPrincipal(login);
-					telaFunc.setLocationRelativeTo(null);
-					telaFunc.setVisible(true);
-					dispose();
+					if (login != null) {
 
-				
-				}else {
-					
-					JOptionPane.showMessageDialog(null, "Senha ou Usuario incorretos!");
-				}
+						MenuPrincipal telaFunc = new MenuPrincipal(login);
+						telaFunc.setLocationRelativeTo(null);
+						telaFunc.setVisible(true);
+						dispose();
+
+					} else {
+
+						JOptionPane.showMessageDialog(null, "Senha ou Usuario incorretos!");
+					}
 				} else {
 
 					JOptionPane.showMessageDialog(null, "Senha ou Usuario não preenchidos!");
 				}
 
-
-
-		
-		btnNewButton.setBounds(163, 190, 249, 23);
-		contentPane.add(btnNewButton);
-		
-		
-
+				
 			}
 		});
 
-		btnNewButton.setBounds(163, 190, 249, 23);
+		btnNewButton.setBounds(162, 364, 249, 23);
 		contentPane.add(btnNewButton);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(Color.DARK_GRAY, 9, true));
+		panel.setBackground(new Color(240, 230, 140));
+		panel.setBounds(71, 22, 435, 585);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+				JLabel lblNewLabel = new JLabel("Login");
+				lblNewLabel.setBounds(34, 67, 351, 71);
+				panel.add(lblNewLabel);
+				lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+				lblNewLabel.setFont(new Font("Yu Gothic Medium", Font.BOLD, 32));
+				lblNewLabel.setForeground(new Color(0, 0, 51));
+				lblNewLabel.setBackground(UIManager.getColor("Button.disabledForeground"));
+				
+				JLabel lblNewLabel_1 = new JLabel("");
+				lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\frete\\Documents\\clinica-medica\\src\\imagens\\icons8-usuário-homem-com-círculo-24.png"));
+				lblNewLabel_1.setBounds(344, 207, 46, 24);
+				panel.add(lblNewLabel_1);
+				
+				JLabel lblNewLabel_2 = new JLabel("");
+				lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\frete\\Documents\\clinica-medica\\src\\imagens\\icons8-particular-2-24.png"));
+				lblNewLabel_2.setBounds(344, 271, 46, 34);
+				panel.add(lblNewLabel_2);
+				
+				JLabel lblNewLabel_3 = new JLabel("");
+				lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\frete\\Documents\\clinica-medica\\src\\imagens\\icons8-login-arredondado-30.png"));
+				lblNewLabel_3.setBounds(344, 335, 36, 34);
+				panel.add(lblNewLabel_3);
 
 	}
 }
 
+class RoundedJTextField extends JTextField {
+	private Shape shape;
+
+	public RoundedJTextField(int size) {
+		super(size);
+		setOpaque(false);
+	}
+
+	protected void paintComponent(Graphics g) {
+		g.setColor(getBackground());
+		g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+		super.paintComponent(g);
+	}
+
+	protected void paintBorder(Graphics g) {
+		g.setColor(getForeground());
+		g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+	}
+
+	public boolean contains(int x, int y) {
+		if (shape == null || !shape.getBounds().equals(getBounds())) {
+			shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+		}
+		return shape.contains(x, y);
+	}
+}
